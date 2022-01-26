ebb0a194af67ae19e6c675fb768197f2cf28d009
Kanhu charan mishra <42674361+kanhu123mishra@users.noreply.github.com>
2022-01-26 20:21:08 +0530

    Update 181.Employees Earning More Than Their Managers.sql

--- Practice_26_01_2022/181.Employees Earning More Than Their Managers.sql
+++ Practice_26_01_2022/181.Employees Earning More Than Their Managers.sql
@@ -1,6 +1,9 @@
-LeetCode SQL query for employee earning more than their Managers:
+#LeetCode SQL query for employee earning more than their Managers:
 
 select e.name as Employee from employee e join 
  employee e2 on e.managerId=e2.id
  where e.salary > e2.salary;
  
+
+#Runtime: 318 ms, faster than 80.02% of MySQL online submissions for Employees Earning More Than Their Managers.
+#Memory Usage: 0B, less than 100.00% of MySQL online submissions for Employees Earning More Than Their Managers.
