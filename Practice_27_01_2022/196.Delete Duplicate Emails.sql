7a22fcfeb95cba34e6badbe7c181548f98f51e89
Kanhu charan mishra <42674361+kanhu123mishra@users.noreply.github.com>
2022-01-27 23:18:16 +0530

    Create 196.Delete Duplicate Emails.sql

---
+++ Practice_27_01_2022/196.Delete Duplicate Emails.sql
@@ -0,0 +1,10 @@
+#196. Delete Duplicate Emails
+
+DELETE p1 FROM Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id;
+    
+    
+#Runtime: 757 ms, faster than 50.84% of MySQL online submissions for Delete Duplicate Emails.
+#Memory Usage: 0B, less than 100.00% of MySQL online submissions for Delete Duplicate Emails.
